e97f86d2c365fef280742b357e6082e2412e8298
josephtmartin <goldglovejo13@gmail.com>
2020-09-08 16:03:00 -0500

    age in dog years

---
+++ practice.js
@@ -0,0 +1,12 @@
+const calculateAgeInDogYears = (age) => {
+  const ageInDogYears = age / 7;
+
+  return ageInDogYears;
+};
+
+const dogAge = calculateAgeInDogYears(22);
+console.log(dogAge);
+
+const dogBreed = (string) => {
+  const 
+};
